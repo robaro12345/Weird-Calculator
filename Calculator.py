e4356b4ff7ee71764f021c1c7bf10ac90029360d
robaro12345 <omkarprajakta@gmail.com>
2023-08-18 21:49:51 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,21 @@
+from time import sleep
+
+
 no_variable = int(input("Hello \n What is the number of variables you will like to calculate? "))
 
-value = int(input("What would you first variable be ?")) 
+value = input("What would you first variable be ?")
+if '.' in value:
+    value = float(value)
+else:
+    value = int(value)
+
 no_variable -= 1
 while no_variable != 0 :
-    calculable_amount = int(input("What is the number you will like  operate with? "))
+    calculable_amount = input("What is the number you will like  operate with? ")
+    if '.' in calculable_amount:
+        calculable_amount = float(calculable_amount)
+    else:
+        calculable_amount = int(calculable_amount)
     operator = input("What would you like operator to be ? please mention from (plus, minus, multi or div).")
     if operator == "plus":
         value += calculable_amount
@@ -21,4 +33,6 @@ while no_variable != 0 :
         value /= calculable_amount
         no_variable -= 1
         print(f"the value now is {value} and the number of variable left are {no_variable}")
+
+
 
